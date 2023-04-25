537f01ec3e977d865d784f535a7c8d03cee88734
brea <brendangugi7@gmail.com>
2023-04-25 14:10:14 -0700

    get_spec

---
+++ get_spec.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *get_spec - looks for a given specifier
+ *@p: argument by get_spec
+ *Return: Null
+ */
+int (*get_spec(char p))(va_list))
+{
+	int i = 0;
+     
+	spec arr[] = {
+		{"c", print_c},
+		{"s", print_string},
+		{"%", print_%},
+		{"d", print_decimal},
+		{"i", print_int},
+		{NULL, NULL}
+	};
+	while (arr[i].valid)
+	{               
+	if (p == arr[i].valid[0])        
+		return(arr[i]);              
+	i++;
+	}
+	return (NULL);
+}
